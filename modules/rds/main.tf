93d990f2e00e8f4155b2bf5557b51930147944e3
TechWorld707 <hento77@yahoo.com>
2026-08-21 15:27:14 +0100

    fix(rds): align force SSL apply method (#16)

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -29,7 +29,7 @@ resource "aws_db_parameter_group" "this" {
   parameter {
     name         = "rds.force_ssl"
     value        = "1"
-    apply_method = "immediate"
+    apply_method = "pending-reboot"
   }
 
   dynamic "parameter" {
